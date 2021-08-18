9bff595e60b87550192fc09acaf572944fd2750f
Farley, Tom <tom.farley@ukaea.uk>
2021-08-18 13:54:25 +0100

    Add initial modules for BB calibration and seq movies

---
+++ ir_tools/calibration/TF_python_tools/calc_calib_coofs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+
+
+Created: 
+"""
+
+import logging
+from typing import Union, Iterable, Sequence, Tuple, Optional, Any, Dict
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+logger.propagate = False
+
+if __name__ == '__main__':
+    pass
