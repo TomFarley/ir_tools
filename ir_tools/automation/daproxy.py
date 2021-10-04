5dd9026c706806abaef0212d183cc35a231cb22c
Farley, Tom <tom.farley@ukaea.uk>
2021-10-04 16:08:36 +0100

    Fix bars import

--- ir_tools/automation/daproxy.py
+++ ir_tools/automation/daproxy.py
@@ -31,8 +31,7 @@ FPATH_DA_PROXY = r'D:/mastda/DAProxy/proxy.exe'
 
 YES = '✓'
 NO = '✕'
-
-
+BARS = '='*10
 
 def run_da_proxy(fpath_da_proxy, windows=True):
     # run 'da_proxy' process to connect to MAST-U central messaging server
@@ -54,106 +53,6 @@ def kill_da_proxy(proc_da_proxy):
     except Exception as e:
         logger.info('kill_da_proxy: ' + NO)
         logger.info('kill_da_proxy: ' + repr(e))
-
-def daproxy_control():
-    REFRESH_TIME_MAIN_LOOP = 5  # sec. The MAST-U Abort state seems to only last for ~10s
-    UPDATE_REMOTE_LOG_EVERY = 50  # loops. No point in updating this too often as Github pages site lag by ~20 min
-    STOP_TIME = datetime.time(20, 10)
-    SEND_DATA_TO_SCRATCH_CAM_NOS = [6, 7, 9, ]
-
-    # ----------------------------------------------------------------------------------------------------------------------
-    # PREAMBLE
-    # ----------------------------------------------------------------------------------------------------------------------
-
-
-    state = None
-    shot = None
-    proc_hic = None
-    hic_log = None
-    mtime_msg_log_0 = 0
-    mtime_config_0 = 0
-    shot_0 = 0
-    hic_running = False
-    loop_cnt = 0
-    logger.info('Starting acquisition loop')
-
-
-
-
-
-    signal.signal(signal.SIGINT, sigint_handler)
-
-    # ----------------------------------------------------------------------------------------------------------------------
-    # MAIN MWI ACQUISITION LOOP
-    # ----------------------------------------------------------------------------------------------------------------------
-    while True:
-        # if MSG_LOG changed, update MAST-U shot + state
-        mtime_msg_log = os.stat(FPATH_MSG_LOG).st_mtime
-        if mtime_msg_log != mtime_msg_log_0:
-            mtime_msg_log_0 = mtime_msg_log
-            shot, state = get_shot(logger=logger), get_state(logger=logger)
-            msg_log_changed = True
-
-        # if (top-level) config changed, update .ini files
-        mtime_config = os.stat(FPATH_CONFIG_TOP).st_mtime
-        if mtime_config != mtime_config_0:
-            mtime_config_0 = mtime_config
-            write_config_ini(shot, logger=logger)
-
-        if shot != shot_0:
-            shot_0 = shot
-            write_config_ini(shot, logger=logger)
-            # kill hic if shot changes while hic_running: we either missed the trigger or it was a trigger-less test shot
-            if hic_running:
-                kill_hic()
-                logger.info('HIC killed: shot number changed')
-
-        if hic_running is not True and state == 'PreShot':  # PreShot comes ~2min before shot
-            arp_out = arm_red_pitaya(logger=logger)
-            # TODO REMOTE RED PITAYA REBOOT IF ARP_OUT != 0
-
-            with open(FPATH_HIC_LOG, 'a') as hic_log:
-                proc_hic = subprocess.Popen(
-                    './run-rfm-mthreaded',
-                    stdout=hic_log,
-                    stderr=hic_log,
-                    shell=True,
-                )
-            hic_running = True
-            logger.info('HIC running')
-
-        # catch end of HIC
-        if hic_running:
-            out = proc_hic.poll()
-            if out is not None:
-                hic_finished()
-                if proc_hic.poll() != 0:
-                    logger.info('HIC: ' + NO)
-                    break  # necessary to break main loop when ctrl + c called in terminal
-                logger.info('HIC: ' + YES)
-                mdsplus_to_hdf5(shot, logger=logger)
-                if freia_access:
-                    send_preview_to_scratch(shot, logger=logger)
-                    send_data_to_scratch(shot, cam_nos=SEND_DATA_TO_SCRATCH_CAM_NOS, logger=logger)
-                update_remote_log(logger=logger)
-                shot = get_shot()
-
-        if hic_running and state == 'Abort':
-            kill_hic()
-            logger.info('HIC killed: MAST-U Abort state')
-
-        if datetime.datetime.now().time() > STOP_TIME:
-            kill_hic()
-            kill_da_proxy()
-            logger.info('>>> GOODNIGHT <<<')
-            update_remote_log(logger=logger)
-            break
-
-        if loop_cnt % UPDATE_REMOTE_LOG_EVERY == 0:
-            update_remote_log(logger=logger)
-
-        loop_cnt += 1
-        time.sleep(REFRESH_TIME_MAIN_LOOP)
 
 def update_estimated_shot_time(state, time_entered_state, shot_time_estimate_prev=None):
     """Improve estimated time shot will run based on typical times states are entered"""
