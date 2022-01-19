b1944f1fae02e71eb172b6be31680a7e01777bd8
Farley, Tom <tom.farley@ukaea.uk>
2022-01-19 12:57:06 +0000

    Add wireframe skeleton file

---
+++ ir_tools/calcam_calibration/overlay_calcam_wireframe.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+
+
+Created: 
+"""
+
+import logging
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+
+def plot_wireframe():
+    NotADirectoryError
+
+def annimate_uda_movie():
+    NotADirectoryError
+
+def annimate_movie_data():
+    NotADirectoryError
+
+if __name__ == '__main__':
+    pass
